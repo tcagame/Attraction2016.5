a3b16d7272ea8e03f7020a18d7abb6724fe83571
okuty <okuty.phone@gmail.com>
2016-09-22 20:25:52 +0900

    Frameworkプロジェクト新規作成

---
+++ Solution/Framework/WinMain.cpp
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+#include "Framework.h"
+#include "Mouse.h"
+
+void main( );
+
+int WINAPI WinMain( HINSTANCE hInstaece, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
+	Framework::initialize( );
+	main( );
+	Framework::getInstance( )->run( );
+	Framework::finalize( );
+	return 0;
+}
